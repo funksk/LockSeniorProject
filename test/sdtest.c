519458b268fcd42b0d6c7451a62afc9b5b89f35c
funksk <mkforever200@gmail.com>
2022-03-21 16:04:43 -0400

    wow

--- test/sdtest.c
+++ test/sdtest.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-#define WRKDIR "./LOCKDATA"
+#define WRKDIR "./LOCKDATA/"
 
 //******STRUCTS********
+
+struct stat st = {0};
+
 typedef struct
 {
 	char pin[10];
@@ -29,6 +35,7 @@ typedef struct
 //file functions
 mastUser loadMasterData();
 users loadUserData();
+void saveMastData(mastUser);
 void saveUser();
 void readUser();
 //individual file functions
@@ -40,20 +47,52 @@ users initUserData();
 
 int main(int argv, char *argv[])
 {
-
+	mastUser master;
+	users user;
 	//initSD();
 	if(chkMasterData())
 		loadMasterData();
 	else
 		initMasterData();
-
+	
 	return 0;
 }
 
-int chkMasterData()
+void saveMastData(mastUser m)
 {
 	FILE *f;
-	if(`)
+	mkdir(WRKDIR, 0700);
+	f = fopen(strcat(WRKDIR, "mast.txt"), "w")
+	fprintf(f, "%s\n",m.name);
+	fprintf(f, "%s\n",m.pin);
+	fprintf(f, "%s",m.fingprint);
+}
 
-	return 0;
+mastUser initMasterData()
+{
+	mastUser master;
+	master.name = "buttman jenkins";
+	master.pin = "1234";
+	saveMastData(master);
+	return master;
+}
+
+mastUser loadMasterData()
+{
+	FILE *f;
+	mastUser m;
+	f = fopen(strcat(WRKDIR, "mast.txt"), "r");
+	fgets(m.name, 50, f);
+	fgets(m.pin, 15, f);
+	fgets(m.fingprint, 1000, f);
+	return m;
+}
+
+//return 0 if there is no dir
+//return 1 if there is a dir
+int chkMasterData()
+{
+	if(stat(WRKDIR, &st) == -1)	//if the dir does not exist
+		return 0;
+	return 1;
 }
